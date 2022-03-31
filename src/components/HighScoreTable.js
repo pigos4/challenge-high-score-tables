4147ee0d35668b6950a5ebb5528269cc49e16cb6
Daniel <pigos487@gmail.com>
2022-04-01 00:29:25 +0100

    Level 4

--- src/components/HighScoreTable.js
+++ src/components/HighScoreTable.js
@@ -1,9 +1,32 @@
+import React,{useState}from 'react'
 import scores from "../info/scores";
 
+const Score =(prop)=>{
+  let scoreSorted;
+  const ascending=(v)=>v.sort((a, b) => a.n.localeCompare(b.n))
+  const descending=(v)=>v.sort((a, b) => b.n.localeCompare(a.n))
+  prop.order==='ascending'?scoreSorted=ascending(prop.score):scoreSorted=descending(prop.score)
+  return scoreSorted.map(({ n, s }, i) => {
+    return (
+      <tr key={i}>
+        <td>{n}</td>
+        <td>{s}</td>
+      </tr>
+    );
+  });
+}
+
 const HighScoreTable = () => {
- 
+  const [order,setOrder]=useState('ascending')
+
   return (
     <div>
+      <button
+        onClick={()=>order === 'ascending'
+            ? setOrder(()=>'descending')
+            : setOrder(()=>'ascending')
+        }
+      >Change order</button>
       <h1>High score per Country</h1>
       <div>
         {scores
@@ -20,16 +43,8 @@ const HighScoreTable = () => {
                     </tr>
                   </thead>
                   <tbody>
-                    {scores
-                      .sort((a, b) => a.n.localeCompare(b.n))
-                      .map(({ n, s }, i) => {
-                        return (
-                          <tr  key={i}>
-                            <td>{n}</td>
-                            <td>{s}</td>
-                          </tr>
-                        );
-                      })}
+                    <Score score={scores} order={order} />
+                    
                   </tbody>
                 </table>
               </div>
